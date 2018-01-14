fa010f3aa80c91a1a2ec292f0b62594aa1d2a9f8
takagotch <dyaccb@gmail.com>
2018-01-14 22:39:17 +0900

    @message ||= MsgConfiguration.new

--- sinatra.rb
+++ sinatra.rb
@@ -4,4 +4,11 @@ get "/" do
   cache_control :public, :must_revalidate, :max_age => 60
   "Hello"
 end
+#def xxx=(value)
+#  @xxx = value
+#end
+#
+#xxx      = "local"  #a local variable
+#self.xxx = "method" #method, substitution
+
 
