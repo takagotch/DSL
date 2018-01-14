8fcdb27dea6961e05700606a25ed2b48ffcd7315
takagotch <dyaccb@gmail.com>
2018-01-14 21:45:51 +0900

    class_eval

--- inner.rb
+++ inner.rb
@@ -3,6 +3,15 @@ attr_writer :name
 attr_accessor :color
 
 class Module
+  def attr_reader(*symbols)
+    symbols.each do |symbol|
+      class_eval %{
+        def #{symbol}
+	  @#{symbol}
+	end
+      }
+    end
+  end
 end
 
 
