44f13b98e28b85752ee414bd833286ebd2a5fd86
takagotch <dyaccb@gmail.com>
2018-01-14 22:14:55 +0900

    set_values(values); get && set

--- csv3.rb
+++ csv3.rb
@@ -14,11 +14,32 @@ class CSVREcord
 
   protect
     def generate_accessors(columns)
-    
+      @columns = columns
+      @columns.each do |col|
+	 @klass.class_eval %{
+	   def #{col}
+	     @#{clo}
+	   end
+
+	   def #{col}= (value)
+	     @#{col} = value
+	   end
+	 }
+      end
     end
 
   def set_values(values)
-  
+    object = @klass.new
+
+    values.length.times do |i|
+      if @columns[i] == COL_ID
+        object.instance_variable_set("@#{columns[i]}", values[i].to_i)
+      else
+        object.instance_variable_set("@#{columns[i]}", values[i])
+      end
+    end
+
+    @records << object
   end
 end
 
