68c9199d165fb3fa1b13bc67cd6dae4c1c308b9b
takagotch <dyaccb@gmail.com>
2018-01-14 22:42:15 +0900

    self; =　変数; method

---
+++ milter.rb
@@ -0,0 +1,9 @@
+security.privillege_mode = false
+security.effective_user  = nil
+security.effective_group = nil
+
+manager.connectoin_spec = "inet:1111@[127.0.0.1]"
+manager.unix_socket_mode = 0660
+manager.unix_socket_group = nil
+
+
